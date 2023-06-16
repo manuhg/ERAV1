4c8fdbb33aa13f11377a67720859fc8cdb463bee
Manu Hegde <manuhegdev@gmail.com>
2023-06-17 00:24:58 +0530

    standard arch base 1

--- S7/models/model_1.py
+++ S7/models/model_1.py
@@ -6,28 +6,37 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.block_1 = nn.Sequential(
-            nn.Conv2d(1, 8, 3),
+            nn.Conv2d(1, 32, 3),
             nn.ReLU(),
-            nn.BatchNorm2d(8),
-            nn.Conv2d(8, 16, 3),
+            nn.Conv2d(32, 64, 3),
             nn.ReLU(),
-            nn.BatchNorm2d(16),
-            nn.MaxPool2d(2, 2))
+            nn.Conv2d(64, 128, 3),
+            nn.ReLU())
+
+        self.tran1 = nn.Sequential(
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(128, 16, 1)
+        )
 
         self.block_2 = nn.Sequential(
             nn.Conv2d(16, 32, 3),
             nn.ReLU(),
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 10, 3),
-            # nn.ReLU()
+            nn.Conv2d(32, 64, 3),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, 3),
+            nn.ReLU(),
+            nn.Conv2d(128, 256, 3),
+            nn.ReLU()
+
         )
 
         self.block_3 = nn.Sequential(
-            nn.AvgPool2d(8, 1)
+            nn.Conv2d(256, 10, 3),
         )
 
     def forward(self, x):
         x = self.block_1(x)
+        x = self.tran1(x)
         x = self.block_2(x)
         x = self.block_3(x)
         x = x.view(-1, 10)
